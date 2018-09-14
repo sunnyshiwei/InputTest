27b106691307f050c2cc766f5f8501bb264d1ac8
shiwei <13998538053@163.com>
2018-09-14 09:28:31 +0800

    +）0913更新代码

--- src/InputPackage/ActionSelenium.java
+++ src/InputPackage/ActionSelenium.java
@@ -1,10 +1,13 @@
 package InputPackage;
 
 
+import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class ActionSelenium {
 
@@ -44,12 +47,7 @@ public class ActionSelenium {
 		user.sendKeys("13998538053");
 		driver.findElement(By.name("password")).sendKeys("ww744934");
 		driver.findElement(By.xpath(".//*[@id='signup-form']/div[5]/input")).click();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.sleepTime(3000);
 	}
 
 	/*
@@ -61,12 +59,7 @@ public class ActionSelenium {
 	 */
 	public void RedioBox() {
 		driver.get("https://www.imooc.com/user/setprofile");
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.sleepTime(3000);
 		/**
 		 * document.getElementById('J_GotoTop').style.display='none'
 		 * 用JS隐藏右侧遮罩
@@ -123,16 +116,13 @@ public class ActionSelenium {
 		driver.findElement(By.id("signup-form")).submit();
 	}
 	/*
-	 * 上传文件*/
+	 * 上传文件 
+	 * 方法一：用js手动改动js代码，使其【更换头像】直接显示
+	 * 且使用sendkeys方法时，标签必须是input*/
 	public void upHeader(){
 		//点击更换头像
 		driver.get("https://www.imooc.com/user/setbindsns");
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.sleepTime(3000);
 		WebElement click_header=driver.findElement(By.linkText("更换头像"));
 		/*由于更换头像是鼠标悬停时才能显示，此时需要手动将<div class="update-avator" style="bottom: -30px;">
 		 * 中 -30px 改为 0px 才可以直接点击更换头像*/
@@ -140,29 +130,116 @@ public class ActionSelenium {
 		String jScrip="document.getElementsByClassName('update-avator')[0].style.bottom='0'";
 		JavascriptExecutor js= (JavascriptExecutor) driver;
 		js.executeScript(jScrip);
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.sleepTime(3000);
 		click_header.click();
-	    try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.sleepTime(3000);
 		//点击上传头像并上传
 		driver.findElement(By.id("upload")).sendKeys("F:\\selenium\\示例图片_03.JPG");
-		  try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		this.sleepTime(3000);
 		//头像上传成功后点击确定按钮
 		driver.findElement(By.linkText("确定")).click();		
+	}
+	/*上传文件
+	 * 方法二：使用鼠标滑动方法
+	 * 且使用sendkeys方法时，标签必须是input*/
+	public void upFile(){
+		driver.get("https://www.imooc.com/user/setbindsns");
+		this.sleepTime(3000);
+		WebElement header= driver.findElement(By.className("avator-img"));
+		Actions action= new Actions(driver);
+		//鼠标滑动方法
+		action.moveToElement(header).perform();	
+		this.sleepTime(2000);
+		driver.findElement(By.linkText("更换头像")).click();
+		this.sleepTime(3000);
+		//点击上传头像并上传
+		driver.findElement(By.id("upload")).sendKeys("F:\\selenium\\示例图片_03.JPG");
+		this.sleepTime(3000);
+		//头像上传成功后点击确定按钮
+		driver.findElement(By.linkText("确定")).click();	
+		
+	}
+	/*上传文件
+	 * 方法三：由于系统文件脚本无法识别，推荐使用AutoIT工具实现文件上传*/
+	public void upFileWithAutoIt(){
+		driver.get("https://www.imooc.com/user/setbindsns");
+		this.sleepTime(3000);
+		WebElement header= driver.findElement(By.className("avator-img"));
+		Actions action= new Actions(driver);
+		//鼠标滑动方法
+		action.moveToElement(header).perform();	
+		this.sleepTime(2000);
+		driver.findElement(By.linkText("更换头像")).click();
+		this.sleepTime(3000);
+		//点击上传头像
+		driver.findElement(By.linkText("上传头像")).click();
+		try {
+			Runtime.getRuntime().exec("F:\\selenium\\TestForUploadFile\\testAutoIt.exe");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//头像上传成功后点击确定按钮
+		driver.findElement(By.linkText("确定")).click();	
+	}
+	/*下拉框操作
+	 * */
+	public void downSelectBox(){
+		driver.get("https://www.imooc.com/user/setprofile");
+		this.sleepTime(3000);
+		//进入个人信息页面
+		driver.findElement(By.linkText("个人信息")).click();
+		this.sleepTime(3000);
+		//点击编辑进入编辑页面
+		driver.findElement(By.className("pull-right")).click();
+		this.sleepTime(2000);
+		
+		WebElement jobSelect= driver.findElement(By.xpath("//form[@id='profile']/div[2]/*/select"));
+		
+		Select jobDownList= new Select(jobSelect);
+		//方法一：通过selectByIndex选择下拉列表中内容
+		//jobDownList.selectByIndex(3);
+		//方法二：通过selectByValue选择下拉列表中内容
+		//jobDownList.selectByValue("10");
+		//方法三：通过selectByVisibleText选择下拉列表中内容
+		jobDownList.selectByVisibleText("Web前端工程师");		
+		System.out.println("验证当前下拉框是否为多选"+jobDownList.isMultiple());
+		//deselectByVisibleText方法只能适用于多选下拉列表
+		//jobDownList.deselectByVisibleText("Web前端工程师");
+		//下拉框操作：getAllSelectedOptions()//getFirstSelectedOption().getText()
+	    //getAllSelectedOptions方法也一般用于多选下拉列表
+		List<WebElement> list=jobDownList.getAllSelectedOptions();	
+		for(WebElement option:list){
+			System.out.println(option.getText());
+		}
+		//获取当前页面选中的值getFirstSelectedOption().getText()
+		jobDownList.getFirstSelectedOption().getText();
+		System.out.println(jobDownList.getFirstSelectedOption().getText());
+	}
+	/*元素进阶 Actions action=new Actions(dirver)
+	 * 鼠标左击：action.click().perform()
+	 * 鼠标右击：action.contextClick().perform()
+	 * 鼠标双击：action.doubleClick().perform()
+	 * 鼠标悬停：action.moveToElement().perform();
+	 * */
+	public void mouseClick(){
+		//WebElement loginElement=driver.findElement(By.id("js-signin-btn"));
+		//将driver装换成action
+		Actions action=new Actions(driver);
+		
+		WebElement loginElement=driver.findElement(By.id("js-signin-btn"));
+		
+		action.click(loginElement).perform();
+
+	}
+	/*等待时间*/
+	public void sleepTime(int sleepTime){
+		try {
+			Thread.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -171,8 +248,12 @@ public class ActionSelenium {
 		action.InitDriver();
 		//action.checkBox();
 		//action.button();
-		action.InputBox();
-		action.upHeader();
+		//action.InputBox();
+		action.mouseClick();
+		//action.downSelectBox();
+		//action.upFileWithAutoIt();
+		//action.upFile();
+		//action.upHeader();
 		//action.webForm();
 		//action.RedioBox();
 	}
